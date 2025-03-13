cff238495a0b67e6d4b774e2214988016ca5e098
aryan1234-coder <aryansachan2017@gmail.com>
2025-03-13 14:38:28 +0530

    Added Photo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
 import '../CssComponents/Header.css';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const backgroundPhoto = document.querySelector('.background-photo');
+      if (backgroundPhoto) {
+        const backgroundPhotoHeight = backgroundPhoto.offsetHeight;
+        if (window.scrollY > backgroundPhotoHeight) {
+          setIsFixed(true);
+        } else {
+          setIsFixed(false);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -18,12 +40,31 @@ const Header = () => {
     }
     setIsMenuOpen(false); // Close the menu after clicking a link
   };
+
+  const headerRef = useRef(null); // Create a reference for the header section
+
+  const scrollToHeader = () => {
+    if (headerRef.current) {
+      headerRef.current.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll to the header
+    }
+  };
   
   return (
-      
+
+    <>
+        <div className="background-photo">
+        <div className="overlay">
+          <h1>Hello, I'm Aryan</h1>
+          <p>Developer, Open Source enthusiast</p>
+          <button onClick={scrollToHeader} className="know-me-button">
+            Get to Know Me :) A
+          </button>
+
+        </div>
+      </div>
 
  
-    <header>
+    <header ref={headerRef} className={`header ${isFixed ? 'fixed' : ''}`}>
     {/* https://drive.google.com/file/d/1nojLf3tyank9OeIE7guk3AOn4cH_QGOX/view?usp=drive_link */}
     {/* https://drive.google.com/file/d/1nojLf3tyank9OeIE7guk3AOn4cH_QGOX/view?usp=drive_link */}
       {/* Left Side: Photo and Name */}
@@ -52,6 +93,7 @@ const Header = () => {
         ☰
       </div>
     </header>
+    </>
     
   );
 };
